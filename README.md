25ee28158121d55670cc028e334b0c7131bacf3e
Fer-Bar <linofernando2703@gmail.com>
2022-03-04 19:28:50 -0300

    Changes in Folder Structure & App Factory Function

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bookmarker-API
+This is an RestApi built with Flask and Flask-RESTful
---
+++ src/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, jsonify
+
+
+
+
+# if __name__ == '__main__':
+#     app.run()
+    
